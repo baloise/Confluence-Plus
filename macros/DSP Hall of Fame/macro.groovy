8e12fb482957d7565840790aea29dba7a1e01ba1
Matthias Cullmann <matthias.cullmann@baloise.ch>
2019-09-20 13:59:55 +0200

    #4 hall of fame

---
+++ macros/DSP Hall of Fame/macro.groovy
@@ -0,0 +1,19 @@
+import com.atlassian.bandana.BandanaManager
+import static com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext.GLOBAL_CONTEXT
+
+private List<Map> loadStats() {
+    String statskeyProp = "default.permissions.stats.key"
+    BandanaManager bandanaManager = com.atlassian.sal.api.component.ComponentLocator.getComponent(BandanaManager.class)
+    (bandanaManager.getValue(GLOBAL_CONTEXT, statskeyProp) ?: []) as List<Map> 
+}
+
+List events = []
+long actual = -1
+loadStats().each{
+    if(it.actual > actual) {
+        if(it.user) events += "<li>${it.date.format('yyyy.MM.dd HH:mm')}: <b>${it.user}</b> added <b>${it.actual-actual}</b></li>"
+        actual = it.actual
+    }
+}
+if(!events) events += '<li>Be the first ;-)</li>'
+"<ul>${events.reverse().join('')}</ul>"
