b9647ddb55516f7c8be679d2a683a790be57ee8e
Matthias Cullmann <matthias.cullmann@baloise.ch>
2020-01-21 18:15:37 +0100

    FIX #5 data classification i18n

--- fragments/contentClassification/fragment.groovy
+++ fragments/contentClassification/fragment.groovy
@@ -11,17 +11,73 @@ import com.atlassian.confluence.user.AuthenticatedUserThreadLocal
 import com.atlassian.confluence.user.ConfluenceUser
 import com.atlassian.confluence.core.ContentPropertyManager
 import com.atlassian.confluence.core.ContentPermissionManager
+import com.atlassian.confluence.languages.LocaleManager;
+
+i18n = [
+	de : [
+		 title : 'Klassifizierung der Vertraulichkeit der Daten',
+		'public' : 'Öffentlich',
+		 internal : 'Intern',
+		 confidential : 'Vertraulich',
+		 secret : 'Geheim',
+		 public_title : 'Öffentliche Informationen',
+		 internal_title : 'Interne Informationen',
+		 confidential_title : 'Vertrauliche Informationen',
+		 secret_title : 'Geheime Informationen',
+		 public_text : ' unterliegen keinen Einschränkungen in der Weitergabe und keinen besonderen Schutzvorkehrungen.',
+		 internal_text : ' stehen grundsätzlich allen Mitarbeitenden der Baloise zur Verfügung.',
+		 confidential_text : ' sind alle für eine spezifische Geschäftstätigkeit erforderlichen Informationen, die nur bestimmten Mitarbeitenden zur Ausübung ihrer Funktion zur Verfügung stehen.',
+		 secret_text : ' stehen nur einem eng begrenzten Kreis von Mitarbeitern zur Verfügung. Ihre Offenlegung hätte gravierende Konsequenzen für die Baloise zur Folge.',
+		 nopermit_title : 'Klassifizierung nicht möglich!',
+		 nopermit_text : 'Geheime Informationen dürfen nicht in Confluence gespeichert werden, da sie Datenträgerverschlüsselung voraussetzen.',
+		 warn_title : 'Vertrauliche Informationen müssen geschützt werden.',
+		 warn_text : 'Bitte schränken Sie den Zugriff auf diese Seite ein.',
+		 update_OK : 'Klassifizierung wurde geändert in',
+		 update_KO : 'Klassifizierung konnte <b>nicht</b> geändert werden.',
+		 apply : 'Speichern',
+	],
+	en : [
+		title : 'Data confidentiality classification',
+		'public' : 'Public',
+		internal : 'Internal',
+		confidential : 'Confidential',
+		secret : 'Secret',
+		public_title : 'Public information',
+		internal_title : 'Internal information',
+		confidential_title : 'Confidential information',
+		secret_title : 'Secret information',
+		public_text : ' is intended for publication to the general public. <br/>It is not subject to any restrictions.',
+		internal_text : ' is available in principle to all Baloise employees. <br/>Release of such information to third parties is only permitted in the course of normal business activities.',
+		confidential_text : 'is information required for a specific business purpose, and is only made available to particular employees entrusted with a related work assignment in order for them to carry out their function.',
+		secret_text : ' is only available to a tightly limited group of employees.',
+		nopermit_title : 'Classification not permitted!',
+		nopermit_text : 'Secret data must not be stored in Confluence, as it requires encrypted storage.',
+		warn_title : 'Confidential information needs to be protected',
+		warn_text : 'Please restrict access to this page.',
+		update_OK : 'Confidentiality classification set to',
+		update_KO : 'Confidentiality classification <b>not</b> updated.',
+		apply : 'Apply',
+	],
+]
+LocaleManager lman = getComponent(LocaleManager.class)
+ConfluenceUser currentUser = AuthenticatedUserThreadLocal.get()
+lang = lman.getLocale(currentUser).toString()[0..1]
+lang = i18n[lang] ? lang : 'en'
+String local(String key) {i18n[lang][key]}
 
 ContentPropertyManager propMan = getComponent(ContentPropertyManager.class)
 
 Page page = context.action.page as Page
 
 
-ConfluenceUser currentUser = AuthenticatedUserThreadLocal.get()
+
+
+
+
 PermissionHelper phelp = new PermissionHelper(
-        getComponent(PermissionManager.class),
-        getComponent(PersonalInformationManager.class),
-        getComponent(PageManager.class)
+		getComponent(PermissionManager.class),
+		getComponent(PersonalInformationManager.class),
+		getComponent(PageManager.class)
 )
 
 String classification = propMan.getStringProperty(page, "com.baloise.classification") ?: "Internal";
@@ -29,12 +85,12 @@ String classification = propMan.getStringProperty(page, "com.baloise.classificat
 String warn = '';
 if('Confidential' == classification && !getComponent(ContentPermissionManager.class).getInheritedContentPermissions(page))
 warn =  'warnUnprotected();'
-if(page.id == 1836221614L) 
+if(page.id == 1836221614L)
 writer.write('''<script>''');
 else
 writer.write('''<section id="dialog-classify" class="aui-dialog2 aui-dialog2-medium aui-layer" role="dialog" aria-hidden="true">
-    <header class="aui-dialog2-header">
-        <h2 class="aui-dialog2-header-main">Data confidentiality classification</h2>
+	<header class="aui-dialog2-header">
+		<h2 class="aui-dialog2-header-main">'''+local('title')+'''</h2>
         <button id="dialog-classify-close" class="aui-dialog2-header-close" aria-label="close" style="background-color:transparent; border :none;">
             <span class="aui-icon aui-icon-small aui-iconfont-close-dialog"></span>
         </button>
@@ -44,40 +100,38 @@ writer.write('''<section id="dialog-classify" class="aui-dialog2 aui-dialog2-med
 <div class="aui-tabs horizontal-tabs">
     <ul class="tabs-menu" id="tabs-classify-all">
         <li class="menu-item">
-            <a href="#tabs-classify-public" id="button-public"><button class="aui-button" style="background-color:#bad80a">Public</button></a>
+            <a href="#tabs-classify-public" id="button-public" data-classification="Public"><button class="aui-button" style="background-color:#bad80a">'''+local('public')+'''</button></a>
         </li>
         <li class="menu-item">
-            <a href="#tabs-classify-internal" id="button-internal"><button class="aui-button" style="background-color:#ffef00">Internal</button></a>
+            <a href="#tabs-classify-internal" id="button-internal" data-classification="Internal"><button class="aui-button" style="background-color:#ffef00">'''+local('internal')+'''</button></a>
         </li>
         <li class="menu-item">
-            <a href="#tabs-classify-confidential" id="button-confidential"><button class="aui-button" style="background-color:#ff8c00; color:white;">Confidential</button></a>
+            <a href="#tabs-classify-confidential" id="button-confidential" data-classification="Confidential"><button class="aui-button" style="background-color:#ff8c00; color:white;">'''+local('confidential')+'''</button></a>
         </li>
         <li class="menu-item">
-            <a href="#tabs-classify-secret" id="button-secret"><button class="aui-button" style="background-color:#a80000; color:white;">Secret</button></a>
+            <a href="#tabs-classify-secret" id="button-secret" data-classification="Secret"><button class="aui-button" style="background-color:#a80000; color:white;">'''+local('secret')+'''</button></a>
         </li>
  </ul>
     <div class="tabs-pane" id="tabs-classify-public">
-        <h2>Public information</h2>
-        <p> is intended for publication to the general public. <br/>It is not subject to any restrictions.</p>
+        <h2>'''+local('public_title')+'''</h2>
+        <p>'''+local('public_text')+'''</p>
     </div>
     <div class="tabs-pane" id="tabs-classify-internal">
-        <h2>Internal information</h2>
-        <p> is available in principle to all Baloise employees. <br/>Release of such information to third parties is only permitted in the course of normal business activities.</p>
+        <h2>'''+local('internal_title')+'''</h2>
+        <p>'''+local('internal_text')+'''</p>
     </div>
     <div class="tabs-pane" id="tabs-classify-confidential">
-        <h2>Confidential information</h2>
-        <p>is information required for a specific business purpose, and is only 
-made available to particular employees entrusted with a related work 
-assignment in order for them to carry out their function.</p>
+        <h2>'''+local('confidential_title')+'''</h2>
+        <p>'''+local('confidential_text')+'''</p>
     </div>
     <div class="tabs-pane" id="tabs-classify-secret">
-        <h2>Secret information</h2>
-        <p> is only available to a tightly limited group of employees.</p>
+        <h2>'''+local('secret_title')+'''</h2>
+        <p>'''+local('secret_text')+'''</p>
         <div class="aui-message aui-message-error" style="background: #FFEBE6">
             <p class="title">
-                <strong>Classification not permitted!</strong>
+                <strong>'''+local('nopermit_title')+'''</strong>
             </p>
-            <p>Confidential data must not be stored in Confluence, as it requires encrypted storage.</p>
+            <p>'''+local('nopermit_text')+'''</p>
         </div>
  </div>
  </div>
@@ -85,7 +139,7 @@ assignment in order for them to carry out their function.</p>
   </div>
     <footer class="aui-dialog2-footer">
         <div class="aui-dialog2-footer-actions">
-            <button id="dialog-classify-submit-button" class="aui-button aui-button-primary">Apply</button>
+            <button id="dialog-classify-submit-button" class="aui-button aui-button-primary">'''+local('apply')+'''</button>
         </div>
     </footer>
 </section>
@@ -113,7 +167,7 @@ function initPageClassification(classification, canEdit) {
 function storePageClassification(classification) {
    var currentClassification = AJS.$("#show-dialog-classify").attr("data-classification");
    if(currentClassification == classification) {
-           console.log("not storing current configuration");
+        console.log("not storing current configuration");
         return false;
    }
    console.log("storing classification " + classification);
@@ -126,7 +180,7 @@ function storePageClassification(classification) {
         AJS.$("#show-dialog-classify").attr("data-classification", classification);
        AJS.flag({
             type: 'success',
-            body: 'Confidentiality classification set to <b>'+classification+'</b>.',
+            body: "'''+local('update_OK')+''' <b>"+clabels.get(classification)+"</b>.",
             close: 'auto',
         }); 
     },
@@ -135,16 +189,22 @@ function storePageClassification(classification) {
         setPageClassification(currentClassification);
         AJS.flag({
             type: 'error',
-            body: 'Confidentiality classification <b>not</b> updated.',
+            body: "'''+local('update_KO')+'''",
             close: 'auto',
         }); 
       }
   });
 }
 
+var clabels = new Map();
+clabels.set("Public", "'''+local('public')+'''");
+clabels.set("Internal", "'''+local('internal')+'''");
+clabels.set("Confidential", "'''+local('confidential')+'''");
+clabels.set("Secret", "'''+local('secret')+'''");
+
 function setPageClassification(classification) {
    if(classification== "Secret") {
-           console.log("ignoring Secret");
+        console.log("ignoring Secret");
         return false;
    }
    var colors = {
@@ -158,11 +218,12 @@ function setPageClassification(classification) {
       "background-color": colors[0],
       "color": colors[1]
     });
-   button.html(classification);
+   button.attr("data-classification-page",classification);
+   button.html(clabels.get(classification));
  }
 
 function warnUnprotected(){
-    var warning= AJS.$('<div class="aui-message aui-message-warning"><p class="title"><strong>Confidential information needs to be protected</strong></p><p>Please restrict access to this page.</p></div>');
+    var warning= AJS.$('<div class="aui-message aui-message-warning"><p class="title"><strong>'''+local('warn_title')+'''</strong></p><p>'''+local('warn_text')+'''</p></div>');
     AJS.$("#main-content").prepend(warning);
 }
 
@@ -174,13 +235,13 @@ function resetClassifyDialog() {
 AJS.$("#dialog-classify-submit-button").on('click', function (e) {
     e.preventDefault();
     AJS.dialog2("#dialog-classify").hide();
-    var classification =  AJS.$("#show-dialog-classify").text();
-     storePageClassification(classification);
+    var classification =  AJS.$("#show-dialog-classify").attr("data-classification-page");
+    storePageClassification(classification);
 });
 
 AJS.$("#tabs-classify-all a").on('click', function (e) {
     AJS.$("#dialog-classify-submit-button").attr("disabled", this.id == "button-secret");
-    setPageClassification(AJS.$(this).text());
+    setPageClassification(AJS.$(this).attr("data-classification"));
 	AJS.tabs.change(AJS.$(this));
 	return false;
 });
