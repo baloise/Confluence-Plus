8ab139b1f23a1bfaa98d406667115ec0acf834b0
Matthias Cullmann <matthias.cullmann@baloise.ch>
2020-07-29 17:59:24 +0200

    SUITI-1793 Monitoring Continuous Integration

---
+++ macros/Timeseries Table/macro.groovy
@@ -0,0 +1,57 @@
+import com.atlassian.bandana.BandanaManager
+import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext
+import static com.atlassian.sal.api.component.ComponentLocator.getComponent
+
+
+CONTEXT = new ConfluenceBandanaContext("com.bal.time.series")
+private ConfluenceBandanaContext CONTEXT(){CONTEXT as ConfluenceBandanaContext}
+bandanaMan = getComponent(BandanaManager.class)
+private BandanaManager bandanaMan(){bandanaMan as BandanaManager}
+
+private TreeMap<Long,Integer> load(String name) {
+	(bandanaMan().getValue(CONTEXT(), name) ?: [:]) as TreeMap<Long,Integer>
+}
+
+private String uniqueKey(String name) { "unIq_€."+name}
+
+private TreeSet<String> loadUnique(String name) {
+    (bandanaMan().getValue(CONTEXT(), uniqueKey(name)) ?: []) as TreeSet<String>
+}
+
+
+TreeSet<Long> dates = new TreeSet()
+
+Set<String> names = parameters.names.split(",").collect{ String name ->
+    name = name.trim()
+    name.endsWith(".unique")? loadUnique(name[0..-8]) : name
+}.flatten() as Set
+
+TreeMap<String,TreeMap<Long,Integer>> series = names.collectEntries{ name ->
+    TreeMap<Long,Integer> data = load(name)
+    dates.addAll(data.keySet())
+    ["$name" : data]
+} as TreeMap
+
+
+String ret = ''' 
+<table>
+ <thead>
+    <tr>
+      <th>Date</th>
+'''
+series.keySet().each{
+    ret += "<th>$it</th>"
+}
+
+ret += '''    </tr>
+  </thead>
+<tbody>'''
+dates.each { date ->
+    ret += "  <tr><td>${(new Date(date)).format('dd-MM-yyyy HH:mm:ss')}</td>"
+    series.values().each{
+        ret += "<td>${it.floorEntry(date)?.value ?: 0}</td>"
+    }
+    ret += "</tr>"
+}
+ret +'''</tbody>
+</table>'''
---
+++ restendpoints/timeseries.groovy
@@ -0,0 +1,83 @@
+import com.onresolve.scriptrunner.runner.rest.common.CustomEndpointDelegate
+import groovy.json.JsonBuilder
+import groovy.transform.BaseScript
+
+import javax.ws.rs.core.MultivaluedMap
+import javax.ws.rs.core.Response
+
+import com.atlassian.bandana.BandanaManager
+import com.atlassian.confluence.setup.bandana.ConfluenceBandanaContext
+import static com.atlassian.sal.api.component.ComponentLocator.getComponent
+import com.atlassian.confluence.setup.bandana.KeyedBandanaContext
+
+@BaseScript CustomEndpointDelegate delegate
+
+CONTEXT = new ConfluenceBandanaContext("com.bal.time.series")
+private ConfluenceBandanaContext CONTEXT(){CONTEXT as ConfluenceBandanaContext}
+bandanaMan = getComponent(BandanaManager.class)
+private BandanaManager bandanaMan(){bandanaMan as BandanaManager}
+private String uniqueKey(String name) { "unIq_€."+name}
+
+private Response ok(entity){
+	Response.ok(new JsonBuilder(entity).toString()).header("Content-Type", "application/json").build()
+}
+
+private TreeMap<Long,Integer> load(String name) {
+	(bandanaMan().getValue(CONTEXT(), name) ?: [:]) as TreeMap<Long,Integer>
+}
+
+private void save(String name, TreeMap<Long,Integer> history) {
+	 bandanaMan().setValue(CONTEXT(), name, history)
+}
+   
+private void reset(String name) {
+	bandanaMan().removeValue(CONTEXT(), name)
+}
+
+private TreeSet<String> loadUnique(String name) {
+    (bandanaMan().getValue(CONTEXT(), uniqueKey(name)) ?: []) as TreeSet<String>
+}
+
+private int unique(String name, String value) {
+	TreeSet<String> values =  loadUnique(name) 
+	if(values.add(value)) bandanaMan().setValue(CONTEXT(), uniqueKey(name), values)
+	return values.size()
+}
+
+private int convert(String name, String value, Integer lastValue) {
+	if(value == "inc") 
+		return lastValue ? ++lastValue : 1
+	if(value.integer) 
+		return value as int
+	return unique(name, value)
+}
+
+private TreeMap<Long,Integer> save(String name, Long date, String value) {
+	TreeMap<Long,Integer> history = load(name)
+    Integer lastValue = history.lastEntry()?.value
+	int converted = convert(name, value, lastValue)
+	if(lastValue != converted) {
+		history.put(date, converted)
+		save(name, history)
+	}
+	return history
+}
+
+timeseries(httpMethod: "GET") { MultivaluedMap<String, String> queryParams, String body ->
+	List<String> names = queryParams.get('name')
+	
+	if(!names) return ok(bandanaMan().getKeys(CONTEXT()))
+	if(queryParams.containsKey("reset")) {
+		names.each{reset(it)}
+		return ok(names.collectEntries{["$it" : [:]]})
+	} 
+	
+	String value = queryParams.getFirst('value')?.trim()
+	if(value) {
+		return ok(names.collectEntries{["$it" : save(it, (queryParams.getFirst('date') ?: System.currentTimeMillis()) as long, value)]})
+	} else if(queryParams.containsKey("unique")) {
+        return ok(names.collectEntries{["$it" : loadUnique(it)]})
+    } else {
+		return ok(names.collectEntries{["$it" : load(it)]})
+	}	
+}
