7cad503d0b5a863b6400542543643b1ee6cd710b
Matthias Cullmann <matthias.cullmann@baloise.com>
2020-08-07 17:23:33 +0200

    WIP

--- restendpoints/githubMirror.groovy
+++ restendpoints/githubMirror.groovy
@@ -14,16 +14,40 @@ import groovyx.net.http.Method
 import static com.atlassian.sal.api.component.ComponentLocator.getComponent
 import com.atlassian.confluence.pages.Page
 import com.atlassian.confluence.pages.PageManager
-
+import com.atlassian.user.UserManager
+import com.atlassian.user.User
+import com.atlassian.confluence.user.ConfluenceUser
 
 @BaseScript CustomEndpointDelegate delegate
+GITHUB_PREFIX = "https://github.com/"
+PATH_SEPERATOR = '|'
 
 pageMan = getComponent(PageManager.class)
 private PageManager pageMan(){pageMan as PageManager}
-GITHUB_PREFIX = "https://github.com/"
 
-private Page savePage(Page parentPage, String pageTitel, String content){
-	Page child = parentPage.children.find{it.title == pageTitel}
+uman = getComponent(UserManager.class)
+private UserManager uman(){uman as UserManager}
+
+lazyConf = null
+private Map<String, String> loadConf(){
+	if(lazyConf) return lazyConf
+    Page page = pageMan().getPage('GITHUB', 'UserMap')
+	lazyConf = new XmlSlurper().parseText("<p>${page.bodyAsString}</p>").table.tbody.tr.collectEntries{[(it.th.toString().trim()) : (it.td.toString().trim())]}
+}
+
+lazyAdmin = null
+User getAdmin(){
+    lazyAdmin ?: (lazyAdmin = uman().getUser('admin'))
+}
+
+User mapUser(String ghUserName ){
+    String mapped = loadConf()[ghUserName]
+    mapped ? uman().getUser(mapped) : admin
+}
+
+private Page savePage(Page parentPage, String pageTitel, String content, User author = admin){
+    Page child = parentPage.children.find{it.title == pageTitel}
+    def saveContext = new com.atlassian.confluence.core.DefaultSaveContext.Builder().updateLastModifier(true).build()
 	if(!child) {
 		child = new Page()
 		child.bodyAsString = content
@@ -31,35 +55,42 @@ private Page savePage(Page parentPage, String pageTitel, String content){
 		child.title = pageTitel
 		child.space = parentPage.space
 		child.parentPage = parentPage
-		child.creator = parentPage.creator
-		pageMan().saveContentEntity(child, null)
+		child.creator = author as ConfluenceUser
+        child.lastModifier = author as ConfluenceUser
+        child.lastModifierName = author.name
+		pageMan().saveContentEntity(child, saveContext)
 		pageMan().movePageAsChild(child, parentPage)
 	} else {
 		if(child.bodyAsString == content) return child
 		pageMan().<Page>saveNewVersion(child, new com.atlassian.confluence.core.Modification<Page>() {
 			public void modify(Page current) {
 				current.bodyAsString = content
+                current.lastModifier = author
+                current.lastModifierName = author.name
 			}
-	   })
+	   }, saveContext)
 	}
 	 return child
 }
 
 private Page getFolder(Page parent, List<String> path, List<String> parentPath = []) {
 	List<String> myPath = parentPath+path[0]
-	Page page = parent.children.find{it.title == path[0]} ?: savePage(parent, myPath.join('/'), "This is a folder")
+	Page page = parent.children.find{it.title == path[0]} ?: savePage(parent, myPath.join(PATH_SEPERATOR), "")
 	  return path.size() == 1 ? page : getFolder(page, path[1..-1], myPath)
 }
 private Page getHomePage() {pageMan().getPage("GITHUB", "Home")}
 
 
-private def writePage(String url, String html) {
+private def writePage(String url, String html, User author) {
 	List<String> path = url.split('/')[0..-2]
-	savePage(getFolder(homePage, path),url,html)
+	Page page = savePage(getFolder(homePage, path),url.replaceAll('/',PATH_SEPERATOR),html, author)
+    getComponent(com.atlassian.confluence.core.ContentPropertyManager.class).setStringProperty(page, "com.baloise.classification", "Public")
+    return page
 }
 
 String scrapePage(String url){
-	 def httpBuilder = withProxy(new HTTPBuilder(GITHUB_PREFIX))
+	//TODO url rewriting
+	def httpBuilder = withProxy(new HTTPBuilder(GITHUB_PREFIX))
 	def gitResp =  httpBuilder.request(Method.GET, ContentType.HTML) {
 		uri.path = url
 	}
@@ -72,24 +103,40 @@ String scrapePage(String url){
 }
 
 private def deletePage(String url) {
-	pageMan().trashPage(pageMan().getPage("GITHUB", url))
+	pageMan().trashPage(pageMan().getPage("GITHUB", url.replaceAll('/',PATH_SEPERATOR)))
 }
-	
+
+boolean isDoc(String path) {path.toLowerCase().endsWith(".md") || path.toLowerCase().endsWith(".adoc")}
+
 githubMirror(httpMethod: "POST") { MultivaluedMap queryParams, String body, HttpServletRequest request ->
 	def json = new JsonSlurper().parseText(body)
 	def resp = body
-	savePage(getFolder(homePage, ["JSON"]), "JSON-"+System.currentTimeMillis(), body)
+	//savePage(getFolder(homePage, ["JSON"]), "JSON-"+System.currentTimeMillis(), body)
 	if(json.pages) {
 		resp = json.pages
 		json.pages.each { page ->
 			String url = page.html_url - GITHUB_PREFIX - '/blob/master'
 			if(["created", "edited"].contains(page.action)) {
-				writePage(url, scrapePage(page.html_url - GITHUB_PREFIX))
+				writePage(url, scrapePage(page.html_url - GITHUB_PREFIX), mapUser(json.sender.login))
 			} else if("deleted" == page.action) {
 				deletePage(url)
 			}
 		}
-	}
+    } else if(json.commits && json.ref == "refs/heads/master") {
+        json.commits.each { commit ->
+            ["added", "modified"].each { commit[it].each{ path ->
+                    if(isDoc(path)){
+                        writePage(json.repository.full_name + '/'+path, scrapePage("https://github.com/${json.repository.full_name}/blob/master/${path}"), mapUser(commit.author.name))            
+                    }
+            }}
+            commit.removed.each { path ->
+                 if(isDoc(path)){
+                     deletePage(json.repository.full_name + '/'+path)
+                 }
+            }
+        }
+    }
+    
 	Response.status(200).entity(resp).header("Content-Type", "application/json").build()
 }
 
