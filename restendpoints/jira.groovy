baloise/Confluence-Plus

commit 0bf4820db727976ed6d0f0bf94f667bef6b8e8ee

copy all request header

restendpoints/jira.groovy
@@ -39,7 +39,8 @@ JaxRsResponse service(MethodType httpMethod, HttpServletRequest request, String 
 	def appLink = appLinkService.getPrimaryApplicationLink(JiraApplicationType)
 	def applicationLinkRequestFactory = appLink.createAuthenticatedRequestFactory()
 	
-	def jiraRequest = applicationLinkRequestFactory.createRequest(httpMethod, request.queryString).addHeader("Content-Type", "application/json")
+	def jiraRequest = applicationLinkRequestFactory.createRequest(httpMethod, request.queryString)
+	request.headerNames.each { String name  -> jiraRequest.addHeader(name,request.getHeader(name)) }
 	if(body) jiraRequest.entity = body
 	
 	Response jiraResp = jiraRequest.executeAndReturn({it} as ReturningResponseHandler)

commit 176bc96622f54bce614795928f92e7fbff47a5e3

documented nasty potential bug
see regression in commit 0bf4820db727976ed6d0f0bf94f667bef6b8e8ee

restendpoints/jira.groovy
@@ -39,7 +39,10 @@ JaxRsResponse service(MethodType httpMethod, HttpServletRequest request, String 
 	def appLink = appLinkService.getPrimaryApplicationLink(JiraApplicationType)
 	def applicationLinkRequestFactory = appLink.createAuthenticatedRequestFactory()
 	
+	// do not copy all headers over. Only JSON request will pass.
+    // Otherwise HTTP status code 503 Service Unavailable will be sent
 	def jiraRequest = applicationLinkRequestFactory.createRequest(httpMethod, request.queryString).addHeader("Content-Type", "application/json")
+	
 	if(body) jiraRequest.entity = body
 	
 	Response jiraResp = jiraRequest.executeAndReturn({it} as ReturningResponseHandler)